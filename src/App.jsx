5141bf217ed20e47110123ce906aab00f6e3ec58
Ric Valenzuela <ricvalenzuelaa@github.com>
2026-04-05 23:22:01 +0800

    Fix import case sensitivity for Layout and Skills components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Layout from './components/layout'
+import Layout from './components/Layout'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
-import Skills from './components/skills'
+import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contacts from './components/Contacts'
 
